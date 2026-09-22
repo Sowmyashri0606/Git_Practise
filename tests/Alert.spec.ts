df5318e1e7ce7718421ba7486db08a4fe8afb4e7
Sowmyashri0606 <sowmyashrisantra@gmail.com>
2026-09-22 19:20:20 +0530

    First Commit

---
+++ tests/Alert.spec.ts
@@ -0,0 +1,30 @@
+import test from '@playwright/test'
+test('Learn Alert', async({page})=>{
+page.goto('https://www.leafground.com/alert.xhtml ')
+page.on('dialog',async(alert)=>{
+const alertType = alert.type()
+console.log(alertType)
+if(alertType === 'prompt'){
+ await alert.accept("Playwright")
+}else{
+    await alert.dismiss()
+}
+})
+await page.locator('(//span[text()="Show"])[5]').click()
+console.log("Alert Handled")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
